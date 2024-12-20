a8bb2113436cdb65d7447ef146cdf90eb2b01861
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-12-20 17:34:56 +0100

    Create sign-in component and configure route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './auth/login/login.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
@@ -10,6 +11,7 @@ export const routes: Routes = [
         children: [
             { path: '', redirectTo: 'login', pathMatch: 'full' },
             { path: 'login', component: LoginComponent },
+            { path: 'sign-in', component: SignInComponent },
         ],
     },
 ];
