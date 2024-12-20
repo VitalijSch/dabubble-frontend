6fb74136b322941f891daebdf6e54fd4260ffe8c
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-12-20 11:18:17 +0100

    Create login component and add route for it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'auth', pathMatch: 'full' },
-    { path: 'auth', component: AuthComponent },
+    { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+    {
+        path: 'auth',
+        component: AuthComponent,
+        children: [
+            { path: '', redirectTo: 'login', pathMatch: 'full' },
+            { path: 'login', component: LoginComponent },
+        ],
+    },
 ];
