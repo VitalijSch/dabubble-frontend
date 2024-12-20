1aba8f4cf43c5df1fea09cde6070e5bed40082d5
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-12-20 10:39:39 +0100

    Add HTML and SCSS for Intro component; create and include global Keyframes.scss for animations

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroComponent } from "./intro/intro.component";
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroComponent],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
