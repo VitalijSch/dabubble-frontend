5e6c787e3cd962587169e4b6531378e3a0badbde
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-12-22 13:34:19 +0100

    Create choose avatar component and add route for it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { ChooseAvatarComponent } from './auth/choose-avatar/choose-avatar.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
@@ -12,6 +13,7 @@ export const routes: Routes = [
             { path: '', redirectTo: 'login', pathMatch: 'full' },
             { path: 'login', component: LoginComponent },
             { path: 'sign-in', component: SignInComponent },
+            { path: 'choose-avatar', component: ChooseAvatarComponent },
         ],
     },
 ];
