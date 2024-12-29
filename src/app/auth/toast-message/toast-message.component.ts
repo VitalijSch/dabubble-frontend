d4826ab8b617727d99dbbd3fdc8d7b76bc7f4cac
Vitalij Schwab <schwab.vitalij@gmail.com>
2024-12-29 17:35:29 +0100

    Implement display message based on current route for toast-message component

--- src/app/auth/toast-message/toast-message.component.ts
+++ src/app/auth/toast-message/toast-message.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ToastMessageService } from '../../services/toast-message/toast-message.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toast-message',
@@ -9,5 +10,23 @@ import { ToastMessageService } from '../../services/toast-message/toast-message.
   styleUrl: './toast-message.component.scss'
 })
 export class ToastMessageComponent {
+  routesToMessages = {
+    'choose-avatar': 'Konto erfolgreich erstellt!',
+    'forgot-password': 'E-Mail gesendet!',
+    'reset-password': 'Anmelden',
+  }
+
   toastMessageService: ToastMessageService = inject(ToastMessageService);
+
+  private router: Router = inject(Router);
+
+  getMessageForRoute(): string {
+    const currentUrl = this.router.url;
+    for (const [route, message] of Object.entries(this.routesToMessages)) {
+      if (currentUrl.includes(route)) {
+        return message;
+      }
+    }
+    return '';
+  }
 }
