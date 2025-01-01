a1fbe5afceec8e95dfb01c602209eb00570fb137
Vitalij Schwab <schwab.vitalij@gmail.com>
2025-01-01 23:13:45 +0100

    Create home,channel,message and new-message component and add route for it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,10 @@ import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { ChooseAvatarComponent } from './auth/choose-avatar/choose-avatar.component';
 import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
+import { HomeComponent } from './home/home.component';
+import { NewMessageComponent } from './home/new-message/new-message.component';
+import { ChannelComponent } from './home/channel/channel.component';
+import { MessageComponent } from './home/message/message.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
@@ -19,5 +23,15 @@ export const routes: Routes = [
             { path: 'forgot-password', component: ForgotPasswordComponent },
             { path: 'reset-password/:id', component: ResetPasswordComponent },
         ],
+    },
+    { path: 'home', redirectTo: 'home/new-message', pathMatch: 'full' },
+    {
+        path: 'home',
+        component: HomeComponent,
+        children: [
+            { path: 'new-message', component: NewMessageComponent },
+            { path: 'channel', component: ChannelComponent },
+            { path: 'message', component: MessageComponent },
+        ]
     },
 ];
