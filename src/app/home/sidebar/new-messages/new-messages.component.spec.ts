f8ca5e10744d801d369375a507c43b0e8c6c4eb7
Vitalij Schwab <schwab.vitalij@gmail.com>
2025-01-13 18:07:12 +0100

    Create channels, new-messages commponent at sidebar and implement HTML and SCSS for sidebar, channels and new-messages component

---
+++ src/app/home/sidebar/new-messages/new-messages.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewMessagesComponent } from './new-messages.component';
+
+describe('NewMessagesComponent', () => {
+  let component: NewMessagesComponent;
+  let fixture: ComponentFixture<NewMessagesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NewMessagesComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(NewMessagesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/home/sidebar/sidebar.component.ts
+++ src/app/home/sidebar/sidebar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ChannelsComponent } from "./channels/channels.component";
+import { NewMessagesComponent } from "./new-messages/new-messages.component";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [ChannelsComponent, NewMessagesComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
