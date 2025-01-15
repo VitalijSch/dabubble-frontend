016ed207fff813858947e7973fca6b1ff673a309
Vitalij Schwab <schwab.vitalij@gmail.com>
2025-01-15 16:15:18 +0100

    Add routerlink for sidebar new-message-container to navigate to new-message path

--- src/app/home/sidebar/sidebar.component.ts
+++ src/app/home/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ChannelsComponent } from "./channels/channels.component";
 import { DirectMessagesComponent } from "./direct-messages/direct-messages.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [ChannelsComponent, DirectMessagesComponent],
+  imports: [ChannelsComponent, DirectMessagesComponent, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
