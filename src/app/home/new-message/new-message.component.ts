2ba333289d01f4566ab4014c79e596df8e6e0168
Vitalij Schwab <schwab.vitalij@gmail.com>
2025-01-15 07:19:28 +0100

    Create create-message and add HTML and SCSS to create-message and to new-message

--- src/app/home/new-message/new-message.component.ts
+++ src/app/home/new-message/new-message.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CreateMessageComponent } from "../create-message/create-message.component";
 
 @Component({
   selector: 'app-new-message',
   standalone: true,
-  imports: [],
+  imports: [CreateMessageComponent],
   templateUrl: './new-message.component.html',
   styleUrl: './new-message.component.scss'
 })
