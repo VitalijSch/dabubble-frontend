08e232c370677dc8a124bfdba3ffe88656d12165
Vitalij Schwab <schwab.vitalij@gmail.com>
2025-05-04 13:59:00 +0200

    Create Intro component

---
+++ src/components/Intro.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Intro() {
+  return (
+    <div className="fixed top-0 right-0 bottom-0 left-0 flex justify-center items-center bg-gradient-to-b from-[#797EF3] to-[#313AE5] animate-fadeOut">
+      <div className="max-w-[470px] min-w-[187px] w-fit h-[184px] flex justify-center items-center relative">
+        <Image
+          className="absolute top-1/2 left-0 -translate-y-1/2 animate-moveLeft"
+          src="/images/intro/logo.svg"
+          alt="logo icon"
+          width={186.98}
+          height={184}
+        />
+        <div className="absolute top-1/2 -left-0 -translate-y-1/2 w-[470px] h-[105px] overflow-hidden">
+          <Image
+            className="w-full h-full translate-x-[-470px] animate-moveRight"
+            src="/images/intro/logo-name.svg"
+            alt="logo name icon"
+            width={470}
+            height={105}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
